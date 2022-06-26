a1a03b9371fc6b74a28d7450ed125d485c9fc445
IrumiDesu7 <ikalam89@gmail.com>
2022-06-26 11:50:44 +0800

    add toggle password button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  const adminUser = {
+    email: 'admin@admin.com',
+    password: 'admin123',
+  };
+
+  const [user, setUser] = useState({ name: '', email: '' });
+  const [error, setError] = useState('');
+
+  const login = (details) => {
+    console.log(details);
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log('Logged in');
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log(
+        "Email or password doesn't match. Please contact your administrator"
+      );
+      setError(
+        "Email or password doesn't match. Please contact your administrator"
+      );
+    }
+  };
+
+  const logout = () => {
+    console.log('Logged out!');
+    setUser({
+      name: '',
+      email: '',
+    });
+    setError('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <div className='getting-started'>
+        <h2>Login</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login so you can continue to experience the best features we provided.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      {user.email === '' ? (
+        <LoginForm login={login} error={error} />
+      ) : (
+        <div className='welcome'>
+          <h2>
+            Welcome <span>{user.name}</span>
+          </h2>
+          <button onClick={logout} className='btn-logout'>
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,79 @@
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
+
+export default function LoginForm(props) {
+  const [details, setDetails] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [passwordType, setPasswordType] = useState('password');
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.login(details);
+  }
+
+  function togglePassword() {
+    setPasswordType((prevType) => {
+      return prevType === 'password' ? 'text' : 'password';
+    });
+  }
+
+  return (
+    <div className='form-container'>
+      <form onSubmit={handleSubmit}>
+        <div className='form-inner'>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Your Name'
+            onChange={handleChange}
+            value={details.name}
+          />
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Your Email'
+            onChange={handleChange}
+            value={details.email}
+          />
+
+          <div className='password-container'>
+            <input
+              type={passwordType}
+              name='password'
+              id='password'
+              placeholder='Your Password'
+              onChange={handleChange}
+              value={details.password}
+            />
+            <button
+              className='password-toggle'
+              type='button'
+              onClick={togglePassword}
+            >
+              View
+            </button>
+          </div>
+          {props.error !== '' && (
+            <div className='error-container'>
+              <p>{props.error}</p>
+            </div>
+          )}
+          <button className='login-btn'>Login</button>
+        </div>
+      </form>
+    </div>
+  );
+}
